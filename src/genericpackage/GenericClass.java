18677196b7e848add432e8200d91bc5a8901bdf4
tsalanski <tsalanski@email.arizona.edu>
2018-11-05 19:09:19 -0700

    test/generic files for project

---
+++ src/genericpackage/GenericClass.java
@@ -0,0 +1,16 @@
+package genericpackage;
+
+//This is just a test class to see if the project is functional when we use git
+//Written by Tommy Salanski
+
+public class GenericClass {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
