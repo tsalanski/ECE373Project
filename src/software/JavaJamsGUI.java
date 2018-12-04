045961dc64f1bef67d275a43135cfb5db2685a4e
wysvince <43325628+wysvince@users.noreply.github.com>
2018-12-03 22:22:24 -0700

    Update JavaJamsGUI.java
    Login GUI update

--- src/software/JavaJamsGUI.java
+++ src/software/JavaJamsGUI.java
@@ -214,6 +214,25 @@ public class JavaJamsGUI extends JFrame {
 	private class loginButtonListener implements ActionListener {
       public void actionPerformed(ActionEvent e) {
          JOptionPane.showMessageDialog(null, "Login!");
+         JTextField username = new JTextField();
+         JTextField password = new JPasswordField();
+         Object[] message = {
+             "Username:", username,
+             "Password:", password
+         };
+
+         int option = JOptionPane.showConfirmDialog(null, message, "Login", JOptionPane.OK_CANCEL_OPTION);
+         if (option == JOptionPane.OK_OPTION) {
+             if (username.getText().equals("h") && password.getText().equals("h")) {
+            	 // user logged in...  goto music venue
+                 System.out.println("Login successful");
+             } else {
+                 System.out.println("login failed");
+             }
+         } else {
+             System.out.println("Login canceled");
+         }
+
       }
 	}
 	
